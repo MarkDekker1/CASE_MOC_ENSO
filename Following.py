6633451b5ce9d15c8deb1bd33604e0981a6a065d
Mark <m.m.dekker@outlook.com>
2017-02-27 13:53:28 +0100

    laptop

--- Following.py
+++ Following.py
@@ -1,8 +1,121 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 27 11:43:49 2017
+# ---------------------------------------------- #
+# LEADING SYSTEM: ENSO => ROBERTS ET AL. 2016
+# ---------------------------------------------- #
 
-@author: mark
-"""
+# Info gained from Robert et al. 2016 and NCD, Ch 8, p 46
+# Still dimensional version used
+# I couple this system with the leading system
+# using the function text(DT)
 
-hoi=3
+# ---------------------------------------------- #
+# PREAMBULE
+# ---------------------------------------------- #
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ---------------------------------------------- #
+# INTEGRATION PARAMETERS
+# ---------------------------------------------- #
+
+tmax=5000.
+dt=0.1
+
+# ---------------------------------------------- #
+# PHYSICAL PARAMETERS (TAB 8.6, NCD, TAB 1, Roberts)
+# ---------------------------------------------- #
+
+Tr0 = 16
+Tr = 29.5
+alpha = 1/180.
+H = 100
+hstar = 62
+eps = 0.11
+L = 15e6
+r = 1/400.
+z0 = 75
+mu = 0.0026
+zeta = 1.3
+kwant = 22
+        
+# ---------------------------------------------- #
+# INITIAL CONDITIONS
+# ---------------------------------------------- #
+        
+T10 = 1
+T20 = 1
+h10 = 1
+
+# ---------------------------------------------- #
+# HELPER EQUATIONS
+# ---------------------------------------------- #
+
+def Tsub(T1,T2,h1):
+    return (Tr+Tr0)/2.-(Tr-Tr0)/2.*np.tanh((H-z0+h1+kwant*(T2-T1))/hstar)
+    
+def tau(T1,T2):
+    return tau_ext(25)+mu*(T2-T1)
+    
+def tau_ext(DT):
+    return DT/100.*mu
+
+# ---------------------------------------------- #
+# DIFFERENTIAL EQUATIONS
+# ---------------------------------------------- #
+
+def dT1(DT,T1,T2,h1):
+    return -alpha*(T1-Tr)-eps*tau(T1,T2)*(T2-T1)
+
+def dT2(DT,T1,T2,h1):
+    return -alpha*(T2-Tr)-zeta*tau(T1,T2)*(T2-Tsub(T1,T2,h1))
+    
+def dh1(DT,T1,T2,h1):
+    return r*(-h1-kwant*(T2-T1)/2.)
+
+# ---------------------------------------------- #
+# TIME INTEGRATION LOOP
+# ---------------------------------------------- #
+
+T1vec=[T10]
+T2vec=[T20]
+Hvec=[h10]
+Cvec=[tau(T10,T20)]
+tvec=[0]
+i=0
+while i<tmax/dt:
+    t=i*dt
+    
+    k1 = dt*dT1(t,T1vec[i],T2vec[i],Hvec[i])
+    k2 = dt*dT1(t+0.5*dt,T1vec[i]+k1*0.5,T2vec[i],Hvec[i])
+    k3 = dt*dT1(t+0.5*dt,T1vec[i]+k2*0.5,T2vec[i],Hvec[i])
+    k4 = dt*dT1(t+dt,T1vec[i]+k3,T2vec[i],Hvec[i])
+    T1new=T1vec[i]+(k1+2.*k2+2.*k3+k4)/6.
+    
+    k1 = dt*dT2(t,T1vec[i],T2vec[i],Hvec[i])
+    k2 = dt*dT2(t+0.5*dt,T1vec[i],T2vec[i]+k1*0.5,Hvec[i])
+    k3 = dt*dT2(t+0.5*dt,T1vec[i],T2vec[i]+k2*0.5,Hvec[i])
+    k4 = dt*dT2(t+dt,T1vec[i],T2vec[i]+k3,Hvec[i])
+    T2new=T2vec[i]+(k1+2.*k2+2.*k3+k4)/6.
+    
+    k1 = dt*dh1(t,T1vec[i],T2vec[i],Hvec[i])
+    k2 = dt*dh1(t+0.5*dt,T1vec[i],T2vec[i],Hvec[i]+k1*0.5)
+    k3 = dt*dh1(t+0.5*dt,T1vec[i],T2vec[i],Hvec[i]+k2*0.5)
+    k4 = dt*dh1(t+dt,T1vec[i],T2vec[i],Hvec[i]+k3)
+    h1new=Hvec[i]+(k1+2.*k2+2.*k3+k4)/6.
+    
+    T1vec.append(T1new)
+    T2vec.append(T2new)
+    Hvec.append(h1new)
+    
+    Cvec.append(tau(T1new,T2new))
+    tvec.append(t)
+    i=i+1
+
+tvec=np.array(tvec)/365.
+
+plt.plot(tvec,T1vec,linewidth=3)
+plt.plot(tvec,T2vec,linewidth=3)
+plt.plot(tvec,np.array(Hvec),linewidth=3)
+plt.legend([r'$T_1$',r'$T_2$',r'$h_1$'],loc='best')
+plt.xlim([0,tvec[-1]])
+#plt.ylim([0,30])
--- Leading.py
+++ Leading.py
@@ -1,8 +1,108 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 27 11:43:49 2017
+# ---------------------------------------------- #
+# LEADING SYSTEM: MOC => STOMMEL (CH 10, p 29)
+# ---------------------------------------------- #
 
-@author: mark
-"""
+# For now I use the dimensional version of Stommel.
+# See page 30 of CH 10 of Nonlinear Climate Dynamics
+# for also a nondimensional version.
+# I force this system using Fs(t)
 
-hoi=3
+# ---------------------------------------------- #
+# PREAMBULE
+# ---------------------------------------------- #
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ---------------------------------------------- #
+# INTEGRATION PARAMETERS
+# ---------------------------------------------- #
+
+tmax=100000.
+dt=1
+
+# ---------------------------------------------- #
+# PHYSICAL PARAMETERS (TAB 10.3, NCD) (IN DAYS)
+# ---------------------------------------------- #
+
+tr = 25.                    # temperature relaxation tscale
+H = 4500.                   # mean ocean depth
+td = 180.*365.              # diffusion time scale
+ta = 29.*365.               # advective time scale
+q = 192e10*3600.*24.        # transport coefficient
+V = 300*4.5*8200.*1e9       # Ocean volume
+alphat = 1e-4               # thermal expansion coefficient
+alphas = 76e-5              # haline contraction coefficient
+S0 = 35                     # Reference salinity
+Th = 25                     # meridional temp diff
+rho0 = 1029.                # Reference density
+F0 = 2.3/365                # Scale freshwater flux
+        
+# ---------------------------------------------- #
+# INITIAL CONDITIONS
+# ---------------------------------------------- #
+        
+DT0 = 0.2
+DS0 = 0.2
+
+# ---------------------------------------------- #
+# HELPER EQUATIONS
+# ---------------------------------------------- #
+
+def Q(Drho):
+    return 1./td+q*Drho**2./(rho0**2*V)
+    
+def Drhof(DT,DS):
+    return 1.-alphat*DT+alphas*DS
+    
+def Fs(t):
+    return min(F0+max(t-200,0)*0.1*F0,5*F0)
+
+# ---------------------------------------------- #
+# DIFFERENTIAL EQUATIONS
+# ---------------------------------------------- #
+
+def dDT(t,DT,DS):
+    return -1./tr*(DT-Th)-Q(Drhof(DT,DS))*DT
+
+def dDS(t,DT,DS):
+    return Fs(t)*S0/H-Q(Drhof(DT,DS))*DS
+
+# ---------------------------------------------- #
+# TIME INTEGRATION LOOP
+# ---------------------------------------------- #
+
+DTvec=[DT0]
+DSvec=[DS0]
+Fvec=[Fs(0)]
+tvec=[0]
+i=0
+while i<tmax/dt:
+    t=i*dt
+    k1 = dt*dDT(t,DTvec[i],DSvec[i])
+    k2 = dt*dDT(t+0.5*dt,DTvec[i]+k1*0.5,DSvec[i])
+    k3 = dt*dDT(t+0.5*dt,DTvec[i]+k2*0.5,DSvec[i])
+    k4 = dt*dDT(t+dt,DTvec[i]+k3,DSvec[i])
+    DTnew=DTvec[i]+(k1+2.*k2+2.*k3+k4)/6.
+    
+    k1 = dt*dDS(t,DTvec[i],DSvec[i])
+    k2 = dt*dDS(t+0.5*dt,DTvec[i],DSvec[i]+k1*0.5)
+    k3 = dt*dDS(t+0.5*dt,DTvec[i],DSvec[i]+k2*0.5)
+    k4 = dt*dDS(t+dt,DTvec[i],DSvec[i]+k3)
+    DSnew=DSvec[i]+(k1+2.*k2+2.*k3+k4)/6.
+    
+    DTvec.append(DTnew)
+    DSvec.append(DSnew)
+    
+    Fvec.append(Fs(t))
+    tvec.append(t)
+    i=i+1
+
+tvec=np.array(tvec)/365.
+
+plt.plot(tvec,DTvec,linewidth=3)
+plt.plot(tvec,DSvec,linewidth=3)
+plt.plot(tvec,np.array(Fvec)*10,linewidth=3)
+plt.legend([r'$\Delta T$',r'$\Delta S$',r'$Fs(t)$'],loc='best')
+plt.xlim([0,tvec[-1]])
+plt.ylim([0,30])
